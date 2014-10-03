bc7450a594b4cd62cfa6dc2836bb583fa2eade77
donat_b <donat@opmbx.org>
2014-10-03 11:53:51 +0400

    Add mod connected_textures and make sure it's loaded after moreblocks

--- init.lua
+++ init.lua
@@ -1,28 +1,46 @@
 local load_time_start = os.clock()
+local function connect_glass(node, img)
+	local tmp = core.registered_nodes[node]
+	if not tmp then
+		return
+	end
+	tmp.tiles = img
+	tmp.drawtype = "glasslike_framed"
+	core.register_node(":"..node, tmp)
+end
 
-local glass_list = {
-	["default:glass"] = {"default_glass.png", "connected_textures_glass_stripes.png"},
-	["default:obsidian_glass"] = {"default_obsidian_glass.png", "connected_textures_invisible.png"},
-
-	["moreblocks:iron_glass"] = {"moreblocks_iron_glass.png", "connected_textures_iron_glass_stripes.png"},
-	["moreblocks:coal_glass"] = {"moreblocks_coal_glass.png", "connected_textures_coal_glass_stripes.png"},
-	["moreblocks:clean_glass"] = {"moreblocks_clean_glass.png", "connected_textures_invisible.png"},
-	["moreblocks:trap_glass"] = {"moreblocks_trap_glass.png", "connected_textures_glass_stripes.png"},
-	["moreblocks:super_glow_glass"] = {"moreblocks_super_glow_glass.png", "connected_textures_glow_glass_stripes.png"},
-
-	["titanium:glass"] = {"titanium_glass.png", "connected_textures_titanium_glass_stripes.png"},
+local d_glass_list = {
+	{"glass", {"default_glass.png", "connected_textures_glass_stripes.png"}},
+	{"obsidian_glass", {"default_obsidian_glass.png", "connected_textures_invisible.png"}}
 }
 
-for node,img in pairs(glass_list) do
-	if minetest.registered_nodes[node] then
-		minetest.override_item(node, {tiles = img, drawtype = "glasslike_framed"})
-	end
+for _,i in ipairs(d_glass_list) do
+	connect_glass("default:"..i[1], i[2])
 end
 
-local time = math.floor(tonumber(os.clock()-load_time_start)*100+0.5)/100
-local msg = "[connected_textures] loaded after ca. "..time
-if time > 0.05 then
-	print(msg)
-else
-	minetest.log("info", msg)
+if core.get_modpath("moreblocks") then
+
+	local m_glass_list = {
+		{"iron_glass", {"moreblocks_iron_glass.png", "connected_textures_iron_glass_stripes.png"}},
+		{"coal_glass", {"moreblocks_coal_glass.png", "connected_textures_coal_glass_stripes.png"}},
+		{"clean_glass", {"moreblocks_clean_glass.png", "connected_textures_invisible.png"}},
+		{"trap_glass", {"moreblocks_trap_glass.png", "connected_textures_glass_stripes.png"}},
+		{"super_glow_glass", {"moreblocks_super_glow_glass.png", "connected_textures_glow_glass_stripes.png"}},
+	}
+
+	for _,i in ipairs(m_glass_list) do
+		connect_glass("moreblocks:"..i[1], i[2])
+	end
+
 end
+
+core.register_node(":default:ice", {
+	description = "Ice",
+	tiles = {"connected_textures_ice.png"},
+	is_ground_content = true,
+	use_texture_alpha = true,
+	paramtype = "light",
+	groups = {cracky=3},
+	sounds = default.node_sound_glass_defaults(),
+})
+print(string.format("[connected_textures] loaded after ca. %.2fs", os.clock() - load_time_start))
